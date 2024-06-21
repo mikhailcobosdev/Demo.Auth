146a6ccd8b5f9799f6566953bd6b0af3b162f2d3
Administrator <mikhail.cobos@portalesit.net>
2024-06-21 11:13:10 -0500

    Creación de la clase cuenta

---
+++ Demo.Auth.Entidades/Cuenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Auth.Entidades
+{
+    internal class Cuenta
+    {
+        public string NumeroCuenta { get; set; }
+        public string Titular { get; set; }
+        public decimal Saldo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
